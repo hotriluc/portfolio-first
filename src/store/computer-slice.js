6c824dab7956b65fa8f0990e46f04411ceae024b
Luc Ho <hotriluc97@gmail.com>
2022-12-27 19:54:27 +0100

    complete logic of moving

--- src/store/computer-slice.js
+++ src/store/computer-slice.js
@@ -31,63 +31,95 @@ const ComputerSlice = createSlice({
     },
 
     moveUp: (state) => {
+      // Save current position as previous
       state.prevCell = { x: state.currentCell.x, y: state.currentCell.y };
 
+      // Move to new row
       state.currentCell.x = mod(
         state.currentCell.x - 1,
         state.galleryDimension.length
       );
+
+      // Compare previous cell position with new row size
+      if (
+        state.prevCell.y >
+        state.galleryDimension[state.currentCell.x].size - 1
+      ) {
+        state.currentCell.y =
+          state.galleryDimension[state.currentCell.x].size - 1;
+      }
+
       state.command = '';
     },
+
     moveDown: (state) => {
+      // Save current position as previous
       state.prevCell = { x: state.currentCell.x, y: state.currentCell.y };
 
+      // Move to new row
       state.currentCell.x = mod(
         state.currentCell.x + 1,
         state.galleryDimension.length
       );
+
+      // Compare previous cell position with new row size
+      if (
+        state.prevCell.y >
+        state.galleryDimension[state.currentCell.x].size - 1
+      ) {
+        state.currentCell.y =
+          state.galleryDimension[state.currentCell.x].size - 1;
+      }
+
       state.command = '';
     },
 
     moveRight: (state) => {
+      // Save current position as previous
       state.prevCell = { x: state.currentCell.x, y: state.currentCell.y };
 
+      // If there are no place to move forward anymore in the row
+      // Move to the new row
       if (
         mod(
-          state.currentCell.y + 1,
-          state.galleryDimension[state.currentCell.x].size
+          state.prevCell.y + 1,
+          state.galleryDimension[state.prevCell.x].size
         ) === 0
       ) {
-        state.currentCell.y = 0;
         state.currentCell.x = mod(
           state.currentCell.x + 1,
           state.galleryDimension.length
         );
+        state.currentCell.y = 0;
       } else {
         state.currentCell.y = mod(
           state.currentCell.y + 1,
           state.galleryDimension[state.currentCell.x].size
         );
       }
+
       state.command = '';
     },
 
     moveLeft: (state) => {
+      // Save current position as previous
       state.prevCell = { x: state.currentCell.x, y: state.currentCell.y };
 
+      // If there are no place to move back anymore in the row
+      // Move to the new row
       if (
         mod(
-          state.currentCell.y - 1,
-          state.galleryDimension[state.currentCell.x].size
+          state.prevCell.y - 1,
+          state.galleryDimension[state.prevCell.x].size
         ) ===
-        state.galleryDimension[state.currentCell.x].size - 1
+        state.galleryDimension[state.prevCell.x].size - 1
       ) {
-        state.currentCell.y =
-          state.galleryDimension[state.currentCell.x].size - 1;
         state.currentCell.x = mod(
           state.currentCell.x - 1,
           state.galleryDimension.length
         );
+        state.currentCell.y =
+          state.galleryDimension[state.currentCell.x].size - 1;
       } else {
         state.currentCell.y = mod(
           state.currentCell.y - 1,
